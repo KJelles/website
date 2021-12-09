337828bc49db45d78b8a2da34c60fcff9fd2cb58
Brian Y <yetterbrian@gmail.com>
2021-12-09 18:27:12 -0500

    Fix upload map test

--- server/src/models/map.js
+++ server/src/models/map.js
@@ -486,7 +486,7 @@ module.exports = {
 	updateInfo: (mapID, mapInfo) => {
 		return MapInfo.update(
 			{
-				youtubeID: mapInfo.youtubeID ?? null,
+				youtubeID: mapInfo.youtubeID || null,
 				description: mapInfo.description
 			},
 			{
